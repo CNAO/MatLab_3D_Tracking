5b1c777274f67270bfafa44955118fdea43187d8
unknown <guglielmo.frisella01@universitadipavia.it>
2022-04-11 17:50:20 +0200

    Updated version of field map generator from opera table file

--- Field/Read_Magnetic_Field_Opera.m
+++ Field/Read_Magnetic_Field_Opera.m
@@ -8,24 +8,24 @@ fileID = fopen(fileName,'r');
 
 temp = fgetl(fileID); 
 t=strsplit(temp);
-nz = str2double(t(2));
-ny = str2double(t(3));
-nx = str2double(t(4));
+nz = str2double(t(1));
+ny = str2double(t(2));
+nx = str2double(t(3));
 
 temp_csv = textscan(fileID,readFormat,'HeaderLines',nHeader);
 fclose(fileID);
 
-x = temp_csv{1};    
-y = temp_csv{2};
-z = temp_csv{3};
+x = temp_csv{1}*1E-3;    
+y = temp_csv{2}*1E-3;
+z = temp_csv{3}*1E-3;
 Bx = temp_csv{4};
 By = temp_csv{5};
 Bz = temp_csv{6};
 
-
+%%
 % Save in matlab format the points as they are
-save('PatchTotalCurved.mat','x','y','z','Bx','By','Bz');
-return
+% save('../Field/Demo30def_1300_EBG097_mesh2_5_Z1900.mat','x','y','z','Bx','By','Bz');
+
 % Re-arragne the coordinate in 3D dimensions (check the number of points in X,Y,Z)
 
 x = reshape(x,nz,ny,nx); x = permute(x,[3 2 1]);
@@ -36,11 +36,11 @@ By = reshape(By,nz,ny,nx); By = permute(By,[3 2 1]);
 Bz = reshape(Bz,nz,ny,nx); Bz = permute(Bz,[3 2 1]);
 
 
+% return
+ save('PatchTotalCurved.mat','x','y','z','Bx','By','Bz');
+% 
+% 
 return
-save('Field_cubic_demo48_SIG.mat','x','y','z','Bx','By','Bz');
-
-
-
 %% Some plots for verirication
 
 figure; hold on; axis equal;
@@ -62,18 +62,17 @@ colormap(flipud(hot)); c = colorbar('EastOutside');
 c.Label.String = 'B [T]'; c.Label.FontSize = 14;
 
 %%
-
+figure;
 xx = reshape(x,1,[]);
 yy = reshape(y,1,[]);
 zz = reshape(z,1,[]);
 Bxx = reshape(Bx,1,[]);
 Byy = reshape(By,1,[]);
 Bzz = reshape(Bz,1,[]);
-step= 10;
+step= 20;
 
 figure; axis equal;
 [h] = fscatter3(xx(1:step:end),yy(1:step:end),zz(1:step:end),Byy(1:step:end), jet_mod);
 xlabel('x');ylabel('y');zlabel('z');
 colorbar
 axis equal;
-
